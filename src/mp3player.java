155142efcf85b3c071570e59398d7243120cd93f
Артем <Hippi_smartik1988>
2017-02-27 16:09:28 +0200

    mp3-player interface

---
+++ src/mp3player.java
@@ -0,0 +1,26 @@
+import javax.swing.*;
+
+/**
+ * Created by Артем on 27.02.2017.
+ */
+public class mp3player {
+
+    private JPanel mainPanel;
+    private JSlider slider1;
+    private JComboBox comboBox1;
+    private JTextField textField1;
+    private JSlider slider2;
+    private JButton backButton;
+    private JButton pauseButton;
+    private JButton playButton;
+    private JButton stopButton;
+    private JButton nextButton;
+    private JList list1;
+    private JButton addButton;
+    private JButton removeButton;
+    private JButton sellectAllButton;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
